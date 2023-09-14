3148f0f66bfd8daa4dca55968c610f435107abfa
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-09-15 04:12:29 +0600

    SplashScreenActivity fully functional

--- app/src/main/java/com/example/barta_a_messenger_app/SplashScreenActivity.java
+++ app/src/main/java/com/example/barta_a_messenger_app/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.barta_a_messenger_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start login activity
+                startActivity(new Intent(SplashScreenActivity.this, LoginPageActivity.class));
+                finish(); // Close the splash activity
+            }
+        }, 1000);
     }
 }

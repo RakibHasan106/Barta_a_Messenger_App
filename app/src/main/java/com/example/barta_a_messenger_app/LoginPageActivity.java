c085f44e555adbe471f5e68c3af6ba1b2a0b59e1
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-09-16 23:54:48 +0600

    connection between splash,login,signup,otp page

--- app/src/main/java/com/example/barta_a_messenger_app/LoginPageActivity.java
+++ app/src/main/java/com/example/barta_a_messenger_app/LoginPageActivity.java
@@ -2,13 +2,29 @@ package com.example.barta_a_messenger_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPageActivity extends AppCompatActivity {
+
+    Button loginButton,signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        loginButton = findViewById(R.id.loginbutton);
+        signupButton = findViewById(R.id.signupbutton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPageActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/barta_a_messenger_app/SignUpActivity.java
+++ app/src/main/java/com/example/barta_a_messenger_app/SignUpActivity.java
@@ -2,13 +2,43 @@ package com.example.barta_a_messenger_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button sendOTPButton,loginButton;
+    EditText phone,name,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        sendOTPButton = findViewById(R.id.otpsendbutton);
+        loginButton = findViewById(R.id.loginbutton);
+
+        phone = findViewById(R.id.phone);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+
+        sendOTPButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SendOTPActivity.class);
+                intent.putExtra("phone",phone.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, LoginPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

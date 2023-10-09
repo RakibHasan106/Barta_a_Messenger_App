943d80702f3ce5357f52e2cce3cf9e3fd3a89ecb
Abtahe103 <abtahe19@gmail.com>
2023-10-09 11:46:37 +0600

    Add Contact Complete

--- app/src/main/java/com/example/barta_a_messenger_app/AddContactAdapter.java
+++ app/src/main/java/com/example/barta_a_messenger_app/AddContactAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,8 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class AddContactAdapter {
-    private ClickListener clickListener;
+public class AddContactAdapter extends RecyclerView.Adapter<AddContactAdapter.MyViewHolder>{
+//    private ClickListener clickListener;
 
     Context context;
     static ArrayList<Contact> list;
@@ -25,16 +24,17 @@ public class AddContactAdapter {
     }
 
     @NonNull
+    @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.contactlist,parent,false);
-        return new MyViewHolder(v,clickListener);
+        return new MyViewHolder(v);
     }
 
 
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Contact contact = list.get(position);
-        holder.name.setText(contact.getName());
-        holder.phone.setText(contact.getPhone());
+        holder.name.setText(contact.getFull_name());
+        holder.phone.setText(contact.getPhone_number());
 
 //        ImageView alertImageView = holder.itemView.findViewById(R.id.danger);
 //
@@ -52,7 +52,7 @@ public class AddContactAdapter {
     public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView name,phone;
 
-        public MyViewHolder(@NonNull View itemView,ClickListener clickListener) {
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.textName);
             phone = itemView.findViewById(R.id.textPhone);
@@ -67,12 +67,12 @@ public class AddContactAdapter {
 //            clickListener.onItemClick(getAdapterPosition());
         }
     }
-    public interface ClickListener{
-        void onItemClick(int position);
-    }
+//    public interface ClickListener{
+//        void onItemClick(int position);
+//    }
 
-    public void setOnItemClickListener(ClickListener clickListener){
-        this.clickListener = clickListener;
-    }
+//    public void setOnItemClickListener(ClickListener clickListener){
+//        this.clickListener = clickListener;
+//    }
 
 }
--- app/src/main/java/com/example/barta_a_messenger_app/ProfileActivity.java
+++ app/src/main/java/com/example/barta_a_messenger_app/ProfileActivity.java
@@ -1,16 +1,24 @@
 package com.example.barta_a_messenger_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    String uid;
     RecyclerView recyclerView;
 
     ArrayList<Contact>list;
@@ -19,9 +27,42 @@ public class ProfileActivity extends AppCompatActivity {
 
     AddContactAdapter adapter;
 
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        uid = mAuth.getCurrentUser().getUid();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Contacts").child(uid);
+        list = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AddContactAdapter(this,list);
+
+        recyclerView.setAdapter(adapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+
+                    Contact contact = dataSnapshot.getValue(Contact.class);
+                    list.add(contact);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }

a0f4fb539bf1fad953f506bd57f4b49f9f2c7d93
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-11-17 01:23:42 +0600

    image send feature added.

--- app/src/main/java/com/example/barta_a_messenger_app/ChatAdapter.java
+++ app/src/main/java/com/example/barta_a_messenger_app/ChatAdapter.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
+import kotlinx.coroutines.channels.Send;
 
 public class ChatAdapter extends RecyclerView.Adapter{
 
@@ -61,12 +67,35 @@ public class ChatAdapter extends RecyclerView.Adapter{
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         MessageModel messageModel = messageModels.get(position);
 
-        if(holder.getClass() == SenderViewHolder.class){
-            ((SenderViewHolder)holder).senderMsg.setText(messageModel.getMessage());
+        if(messageModel.isItImage==false){
+            if(holder.getClass() == SenderViewHolder.class){
+                ((SenderViewHolder)holder).sentImage.setVisibility(View.GONE);
+                ((SenderViewHolder)holder).senderMsg.setVisibility(View.VISIBLE);
+                ((SenderViewHolder)holder).senderMsg.setText(messageModel.getMessage());
+                ((SenderViewHolder)holder).senderTime.setText(new SimpleDateFormat("HH:mm").format(new Date(messageModel.getTimestamp())));
+            }
+            else{
+                ((RecieverViewHolder)holder).receivedImage.setVisibility(View.GONE);
+                ((RecieverViewHolder)holder).receiverMsg.setVisibility(View.VISIBLE);
+                ((RecieverViewHolder)holder).receiverMsg.setText(messageModel.getMessage());
+                ((RecieverViewHolder)holder).receiverMsg.setText(new SimpleDateFormat("HH:mm").format(new Date(messageModel.getTimestamp())));
+            }
         }
         else{
-            ((RecieverViewHolder)holder).receiverMsg.setText(messageModel.getMessage());
+            if(holder.getClass() == SenderViewHolder.class){
+                ((SenderViewHolder)holder).senderMsg.setVisibility(View.GONE);
+                ((SenderViewHolder)holder).sentImage.setVisibility(View.VISIBLE);
+                Picasso.get().load(messageModel.getMessage()).into(((SenderViewHolder)holder).sentImage);
+                ((SenderViewHolder)holder).senderTime.setText(new SimpleDateFormat("HH:mm").format(new Date(messageModel.getTimestamp())));
+            }
+            else{
+                ((RecieverViewHolder)holder).receiverMsg.setVisibility(View.GONE);
+                ((RecieverViewHolder)holder).receivedImage.setVisibility(View.VISIBLE);
+                Picasso.get().load(messageModel.getMessage()).into(((RecieverViewHolder)holder).receivedImage);
+                ((RecieverViewHolder)holder).receiverMsg.setText(new SimpleDateFormat("HH:mm").format(new Date(messageModel.getTimestamp())));
+            }
         }
+
     }
 
     @Override
@@ -77,21 +106,26 @@ public class ChatAdapter extends RecyclerView.Adapter{
     public class RecieverViewHolder extends RecyclerView.ViewHolder{
         TextView receiverMsg, receiverTime;
 
+        ImageView receivedImage;
+
         public RecieverViewHolder(@NonNull View itemView) {
             super(itemView);
 
             receiverMsg = itemView.findViewById(R.id.receiverText);
             receiverTime = itemView.findViewById(R.id.receiverTime);
+            receivedImage = itemView.findViewById(R.id.received_image);
         }
     }
 
     public class SenderViewHolder extends RecyclerView.ViewHolder {
         TextView senderMsg,senderTime;
+        ImageView sentImage;
         public SenderViewHolder(@NonNull View itemView) {
             super(itemView);
 
             senderMsg = itemView.findViewById(R.id.senderText);
             senderTime = itemView.findViewById(R.id.senderTime);
+            sentImage = itemView.findViewById(R.id.sent_image);
         }
     }
 }
--- app/src/main/java/com/example/barta_a_messenger_app/InboxActivity.java
+++ app/src/main/java/com/example/barta_a_messenger_app/InboxActivity.java
@@ -1,27 +1,47 @@
 package com.example.barta_a_messenger_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 public class InboxActivity extends AppCompatActivity {
 
@@ -36,7 +56,15 @@ public class InboxActivity extends AppCompatActivity {
     EditText inputMessage;
 
     FirebaseDatabase database;
+    ImageButton imageSendButton;
 
+    String checker="",myUrl="";
+    Uri imagePath;
+    String imageUrl;
+
+    String senderRoom,receiverRoom,senderId,receiverId;
+
+    ArrayList<MessageModel> messageModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +81,13 @@ public class InboxActivity extends AppCompatActivity {
         sendButton = findViewById(R.id.send);
         inputMessage = findViewById(R.id.inputMessage);
 
+        imageSendButton = findViewById(R.id.image_send_button);
+
         database = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
-        String senderId = mAuth.getCurrentUser().getUid();
-        String receiverId = getIntent().getStringExtra("contact_uid").toString();
+        senderId = mAuth.getCurrentUser().getUid();
+        receiverId = getIntent().getStringExtra("contact_uid").toString();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +97,7 @@ public class InboxActivity extends AppCompatActivity {
             }
         });
 
-        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        messageModels = new ArrayList<>();
         final ChatAdapter chatAdapter = new ChatAdapter(messageModels,this,receiverId);
 
         chatRecyclerView.setAdapter(chatAdapter);
@@ -75,8 +105,8 @@ public class InboxActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         chatRecyclerView.setLayoutManager(layoutManager);
 
-        final String senderRoom = senderId + receiverId;
-        final String receiverRoom = receiverId + senderId;
+        senderRoom = senderId + receiverId;
+        receiverRoom = receiverId + senderId;
 
         database.getReference().child("chats")
                         .child(senderRoom)
@@ -90,6 +120,7 @@ public class InboxActivity extends AppCompatActivity {
                                             messageModels.add(model);
                                         }
                                         chatAdapter.notifyDataSetChanged();
+                                        chatRecyclerView.scrollToPosition(messageModels.size()-1);
                                     }
 
                                     @Override
@@ -98,35 +129,198 @@ public class InboxActivity extends AppCompatActivity {
                                     }
                                 });
 
+        RecyclerView.ItemAnimator animator = chatRecyclerView.getItemAnimator();
+        if (animator instanceof DefaultItemAnimator){
+            DefaultItemAnimator defaultItemAnimator = (DefaultItemAnimator) animator;
+            defaultItemAnimator.setAddDuration(1000);
+        }
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String message = inputMessage.getText().toString();
-                final MessageModel model = new MessageModel(senderId,message);
-                model.setTimestamp(new Date().getTime());
-                inputMessage.setText("");
+                if(!message.isEmpty()){
+                    final MessageModel model = new MessageModel(senderId,message);
+                    model.setTimestamp(new Date().getTime());
+                    inputMessage.setText("");
 
-                database.getReference().child("chats")
-                        .child(senderRoom)
-                        .push()
-                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                database.getReference().child("chats")
-                                        .child(receiverRoom)
-                                        .push()
-                                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void unused) {
 
-                                            }
-                                        });
-                            }
-                        });
+                    database.getReference().child("chats")
+                            .child(senderRoom)
+                            .push()
+                            .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+//                                chatRecyclerView.refreshDrawableState();
+                                    chatRecyclerView.scrollToPosition(messageModels.size()-1);
+                                    database.getReference().child("chats")
+                                            .child(receiverRoom)
+                                            .push()
+                                            .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+
+                                                }
+                                            });
+                                }
+                            });
+                }
+
+            }
+        });
+
+        imageSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence options[] = new CharSequence[]
+                        {
+                                "Images",
+                                "PDF Files",
+                                "MS Word Files"
+                        };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(InboxActivity.this);
+                builder.setTitle("Select the File");
+
+                builder.setItems(options, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(i==0){
+                            checker="image";
+
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            intent.setType("image/*");
+                            startActivityForResult(intent.createChooser(intent,"Select Image"),123);
+                        }
+                        else if(i==1){
+                            checker="pdf";
+                        }
+                        else{
+                            checker="doc";
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
+
+//        chatRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                chatRecyclerView.scrollToPosition(messageModels.size()-1);
+//                chatRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//            }
+//        });
+
+        inputMessage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(hasFocus){
+                    chatRecyclerView.scrollToPosition(messageModels.size()-1);
+                }
             }
         });
 
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,@Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==123 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imagePath = data.getData();
+            if(!checker.equals("image")){
+                
+            }
+            else if(checker.equals("image")){
+                uploadImage();
+
+
+
+            }
+            else if(checker.equals("pdf")){
+
+            }
+            else if(checker.equals("doc")){
+
+            }
+            else{
+                Toast.makeText(this,"Nothing Selected,Error",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading....");
+        progressDialog.show();
+
+        FirebaseStorage.getInstance().getReference("chat_images/"+ UUID.randomUUID().toString()).putFile(imagePath).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    task.getResult().getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()){
+                                imageUrl = task.getResult().toString();
+//                                Toast.makeText(InboxActivity.this, imageUrl, Toast.LENGTH_SHORT).show();
+
+                                MessageModel model = new MessageModel(senderId,imageUrl,true);
+                                model.setTimestamp(new Date().getTime());
+
+                                database.getReference().child("chats")
+                                        .child(senderRoom)
+                                        .push()
+                                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+//                                chatRecyclerView.refreshDrawableState();
+                                                chatRecyclerView.scrollToPosition(messageModels.size()-1);
+                                                database.getReference().child("chats")
+                                                        .child(receiverRoom)
+                                                        .push()
+                                                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                            @Override
+                                                            public void onSuccess(Void unused) {
+                                                                progressDialog.dismiss();
+                                                            }
+                                                        });
+                                            }
+                                        });
+
+//                                updateProfilePicture(task.getResult().toString());
+                            }
+
+                        }
+
+                    });
+//                    Toast.makeText(ProfileActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+                }
+                else {
+//                    Toast.makeText(ProfileActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
+
+    }
+
+    private void getImageInImageView() {
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagePath);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+//        imgProfile.setImageBitmap(bitmap);
     }
 }

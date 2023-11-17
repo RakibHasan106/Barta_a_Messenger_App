1d582b3b6474635282637ba52d8abc6588e5acbb
Abtahe103 <abtahe19@gmail.com>
2023-11-17 15:40:55 +0600

    recyclerview added in chat fragment

--- app/src/main/java/com/example/barta_a_messenger_app/chatFragment.java
+++ app/src/main/java/com/example/barta_a_messenger_app/chatFragment.java
@@ -1,20 +1,46 @@
 package com.example.barta_a_messenger_app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class chatFragment extends Fragment {
     private FloatingActionButton fab;
+    RecyclerView recyclerView;
 
+    FirebaseAuth mAuth ;
+    FirebaseUser user;
 
+    private ContactAdapter adapter;
+
+    private ArrayList<Contact> list;
+
+    DatabaseReference databaseReference,userRef;
+    String uid;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -22,6 +48,58 @@ public class chatFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
         fab=view.findViewById(R.id.fab_button);
+        recyclerView=view.findViewById(R.id.recyclerView);
+
+        mAuth = FirebaseAuth.getInstance();
+        uid = mAuth.getCurrentUser().getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("user").child(uid);
+
+        list = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        adapter = new ContactAdapter(requireContext(), list);
+        recyclerView.setAdapter(adapter);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Contacts").child(uid);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Contact contact = dataSnapshot.getValue(Contact.class);
+                    list.add(contact);
+
+                    String uid2 = contact.getUid();
+                    DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("user").child(uid2);
+
+                    userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                String profilePictureUrl = dataSnapshot.child("profilePicture").getValue(String.class);
+                                contact.setProfilePic(profilePictureUrl);
+                                adapter.notifyDataSetChanged();  // Notify adapter to update the UI
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle errors
+                        }
+                    });
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle errors
+            }
+        });
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
